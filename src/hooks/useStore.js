7fdd1e1958aef6d527eedd8fabb8fd60315cd737
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-08 09:08:21 -0700

    Adding useStore hook, will modify once we fetch data

---
+++ src/hooks/useStore.js
@@ -0,0 +1,21 @@
+import create from "zustand";
+
+export const useStore = create((set, get) => ({
+  products: [],
+  cart: [],
+  getProducts: (allProducts) => set({ products: allProducts }),
+  addProduct: (product) => set({ products: [...get().products, product] }),
+  removeProduct: (requestedProduct) => {
+    const products = get().products;
+    let currentProducts = products.filter(
+      (product) => product.id !== requestedProduct.id
+    );
+    set({ products: currentProducts });
+  },
+  addToCart: (product) => set({ cart: [...get().cart, product] }),
+  removeFromCart: (requestedProduct) => {
+    const cart = get().cart;
+    let inCart = cart.filter((product) => product.id !== requestedProduct.id);
+    set({ cart: inCart });
+  },
+}));
