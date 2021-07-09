1182351a5bee983892cb35dacd00311baf990557
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-09 06:38:11 -0700

    added 404 for no route

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,7 +1,32 @@
-import * as React from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { ErrorButton } from "../styles/Styles";
+import { useLocation, useHistory } from "react-router-dom";
+import ErrorImage from "../assets/404.jpeg";
 
 function NoMatch() {
-  return <div>Nothing here</div>;
+  const location = useLocation();
+  const history = useHistory();
+
+  function handleBackClick() {
+    history.push("/");
+  }
+  return (
+    <div
+      css={css`
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      <img src={ErrorImage} width="50%" alt="404 Bee" />
+      <h3>{location.pathname}</h3>
+      <ErrorButton onClick={handleBackClick}>Home</ErrorButton>
+    </div>
+  );
 }
 
 export default NoMatch;
